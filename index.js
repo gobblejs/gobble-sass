5160807657bbd944b5bf48c5d9f7d326af1b4ae4
Rich Harris <richard.a.harris@gmail.com>
2014-08-27 12:29:51 -0400

    update to 0.5.0 API, bump version -> 0.2.0

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-module.exports = function sass ( srcDir, destDir, options, done ) {
+module.exports = function sass ( srcDir, destDir, options, done, err ) {
 	require( 'node-sass' ).render({
-		file: require( 'path' ).join( srcDir, options.src ),
+		file: require( 'path' ).resolve( srcDir, options.src ),
 		success: function ( css ) {
-			require( 'gobble' ).file.write( destDir, options.dest, css ).then( done, done );
+			require( 'gobble' ).file.write( destDir, options.dest, css ).then( done, err );
 		},
-		error: done
+		error: err
 	});
 };
