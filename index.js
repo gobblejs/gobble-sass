de8b260bb400ad458a877a0e98486dde4061ca79
Rich Harris <richard.a.harris@gmail.com>
2014-10-15 15:18:44 -0400

    use all node-sass options (#1)

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ module.exports = function sass ( inputdir, outputdir, options, callback ) {
 	var path = require( 'path' ),
 		sander = require( 'sander' );
 
-	require( 'node-sass' ).render({
-		file: path.join( inputdir, options.src ),
-		sourceComments: 'map',
-		sourceMap: options.dest + '.map',
-		success: function ( css, map ) {
-			// pending https://github.com/sass/node-sass/issues/363...
-			map = JSON.parse( map );
-			map.sourcesContent = map.sources.map( function ( source ) {
-				return sander.readFileSync( source ).toString();
-			});
+	if ( !options.src || !options.dest ) {
+		throw new Error( 'gobble-sass requires `options.src` and `options.dest` to be set' );
+	}
 
-			sander.Promise.all([
-				sander.writeFile( outputdir, options.dest + '.map', JSON.stringify( map ) ),
-				sander.writeFile( outputdir, options.dest, css )
-			]).then( function () { callback(); }, callback );
-		},
-		error: callback
-	});
+	options.file = path.join( inputdir, options.src );
+	options.sourceComments = 'map';
+	options.sourceMap = options.dest + '.map';
+	options.error = callback;
+
+	options.success = function ( css, map ) {
+		// pending https://github.com/sass/node-sass/issues/363...
+		map = JSON.parse( map );
+		map.sourcesContent = map.sources.map( function ( source ) {
+			return sander.readFileSync( source ).toString();
+		});
+
+		sander.Promise.all([
+			sander.writeFile( outputdir, options.dest + '.map', JSON.stringify( map ) ),
+			sander.writeFile( outputdir, options.dest, css )
+		]).then( function () { callback(); }, callback );
+	};
+
+	require( 'node-sass' ).render( options );
 };
